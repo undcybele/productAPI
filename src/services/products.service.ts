84e5a0a9a2380cf62460ce5a6c88db8da0f3df25
undcybele <andreea1bolea@gmail.com>
2024-09-29 15:05:18 +0300

    add functionality to service methods

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,21 +1,47 @@
 import {IProduct} from "../models/products.model";
+import {ProductDataSource} from "../db_config/data-source";
+import {ProductEntity} from "../entities/product.entities";
+import {Repository} from "typeorm";
+import {CreateProductDto, SearchProductDto} from "../dtos/product.dto";
 
-export const getProducts = async (): Promise<IProduct[]> => {
-    return {} as IProduct[];
-};
+export class ProductsService {
+    private productRepository: Repository<ProductEntity>;
+    constructor() {
+        this.productRepository = ProductDataSource.getRepository(ProductEntity);
+    }
 
-export const getProduct = async (id: string): Promise<IProduct | null> => {
-    return {} as IProduct;
-};
+    async getProducts(query?: SearchProductDto): Promise<ProductEntity[]>{
+        console.log('query', query);
+        if(query == undefined) return await this.productRepository.find();
+        return await this.productRepository.find(
+            {
+                take: query.limit,
+                skip: query.page,
+            }
+        );
+    };
 
-export const addProduct = async (newProduct: IProduct): Promise<IProduct> => {
-    return {} as IProduct;
-};
+    async getProductById(id: string): Promise<ProductEntity | null> {
+        if (!id) {return null;}
+        return await this.productRepository.findOneBy({id: Number(id)});
+    }
 
-export const updateExistingProduct = async (id: string, updatedProduct: IProduct): Promise<IProduct | null> => {
-    return {} as IProduct;
-};
+    async addProduct(newProduct: CreateProductDto): Promise<ProductEntity | null> {
+        if(!newProduct) return null;
+        return this.productRepository.save(newProduct);
+    }
 
-export const deleteExistingProduct = async (id: string): Promise<boolean> => {
-    return true;
-};
+    async updateExistingProduct (id: string, productUpdates: IProduct): Promise<IProduct | null> {
+        if(!productUpdates || !id) return null;
+        const productToBeUpdated = await this.getProductById(id);
+        if(!productToBeUpdated) return null;
+        const updatedProduct = this.productRepository.merge(productToBeUpdated, productUpdates);
+        return await this.productRepository.save(updatedProduct);
+    };
+
+    async deleteExistingProduct (id: string): Promise<boolean | null> {
+        if(!id) return null;
+        const result = await this.productRepository.delete(id);
+        return result.affected !== 0;
+    };
+}
